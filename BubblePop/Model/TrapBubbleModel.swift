fcbfb1b36c290ed57537c41bfba9358d3bfc232e
kanethuong <kanethuong@gmail.com>
2024-05-01 21:19:19 +1000

    final commit

---
+++ BubblePop/Model/TrapBubbleModel.swift
@@ -0,0 +1,8 @@
+//
+//  TrapBubbleModel.swift
+//  BubblePop
+//
+//  Created by Nguyễn Trần Trang Anh on 26/4/2024.
+//
+
+import Foundation
---
+++ BubblePop/View/CountdownView.swift
@@ -0,0 +1,18 @@
+//
+//  CountdownView.swift
+//  BubblePop
+//
+//  Created by Nguyễn Trần Trang Anh on 26/4/2024.
+//
+
+import SwiftUI
+
+struct CountdownView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountdownView()
+}
